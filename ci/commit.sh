f15004cd9e7edd1b10e08cb48579fdd16e4843f5
Seth Baxter <seth@neuralux.com>
2016-08-18 13:45:14 -0400

    Automated Release -  [0.0.1]

--- ci/commit.sh
+++ ci/commit.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
 pushd `dirname $0` > /dev/null
-root=$(pwd -P)
+root=$(pwd -P)/..
 popd > /dev/null
 
 git -C $root add \*
+git -C $root add
 git -C $root commit -m "Automated Release - $date [$tag]"
 [ -n "$tag" ] && git tag -am "Version $tag" ${tag}
