ebecff1d45932890d1e66f01866c966b08d093f4
Seth Baxter <seth@neuralux.com>
2016-08-22 14:32:16 -0400

    Automated Release -  []

--- ci/release.sh
+++ ci/release.sh
@@ -6,18 +6,6 @@ popd > /dev/null
 
 source $root/ci/vars.sh
 
-git submodule update --init
-
-if [[ -n "$component_revision" && -n "$component" ]]; then
-  git="git -C $root/src/$component_revision"
-
-  $git checkout master
-  $git pull origin master
-  $git reset --hard $component_revision
-fi
-
-outfile=$root/public/$APP.$EXT
-[ -z "$tag" ] && version=$(git describe --long --tags --always) || version=$tag
 
 full_branch=$(git symbolic-ref HEAD)
 [ -z $full_branch ] && full_branch=$(git describe --contains --all HEAD)
@@ -27,7 +15,21 @@ echo $branch | grep -q id && id="-rc"
 echo $branch | grep -q ci && id="-ci"
 
 git -C $root checkout $branch
+git pull origin $branch
+git submodule update --init
 
+
+# Update the component
+if [[ -n "$component_revision" && -n "$component" ]]; then
+  git="git -C $root/src/$component_revision"
+
+  $git checkout master
+  $git pull origin master
+  $git reset --hard $component_revision
+fi
+
+
+# Build release manifest
 out="{\"version\": \"${version}${id}\",\"components\":{"
 
 sep=
@@ -39,8 +41,13 @@ for app in `cat $root/config/repos.txt` ; do
 done
 
 out="${out}}}"
+outfile=$root/public/$APP.$EXT
 
 echo $out > $outfile
+
+
+# Commit and Push
+[ -z "$tag" ] && version=$(git describe --long --tags --always) || version=$tag
 
 git -C $root add \*
 git -C $root commit -m "Automated Release - $date [$tag]"
